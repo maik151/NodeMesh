1ab6a5e6296a80186bf673ef6ad86248720d9940
Maik <mj4791256@gmail.com>
2026-03-09 12:08:35 -0500

    fix(auth): actualizar Client ID al nuevo OAuth app NodeMesh

--- nodemesh-app/src/environments/environment.prod.ts
+++ nodemesh-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Production environment variables
 export const environment = {
     production: true,
-    googleClientId: '847473990011-mdg8iobgp83sgubolv0hbr17vcdjs5tl.apps.googleusercontent.com'
+    googleClientId: '847473990011-fs1cvf7i0q8fqlmn4ruja1hl8me062jd.apps.googleusercontent.com'
 };
--- nodemesh-app/src/environments/environment.ts
+++ nodemesh-app/src/environments/environment.ts
@@ -3,5 +3,5 @@
 // The Google Client ID is safe to include as it is a public identifier
 export const environment = {
     production: false,
-    googleClientId: '847473990011-mdg8iobgp83sgubolv0hbr17vcdjs5tl.apps.googleusercontent.com'
+    googleClientId: '847473990011-fs1cvf7i0q8fqlmn4ruja1hl8me062jd.apps.googleusercontent.com'
 };

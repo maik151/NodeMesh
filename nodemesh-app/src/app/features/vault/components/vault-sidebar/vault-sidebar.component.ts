4eb413028c01b8a164d35b5a548ae373138298dd
Maik <mj4791256@gmail.com>
2026-04-06 14:45:11 -0500

    style(vault): reemplazar icono de hamburguesa por icono de sidebar personalizado
    - Se actualiza el SVG del botón de colapso en VaultSidebarComponent para usar el icono de estructura de sidebar solicitado por el usuario.

--- nodemesh-app/src/app/features/vault/components/vault-sidebar/vault-sidebar.component.ts
+++ nodemesh-app/src/app/features/vault/components/vault-sidebar/vault-sidebar.component.ts
@@ -21,7 +21,7 @@ import { FolderTheme, QuizSession } from '../../../../core/models/node.model';
           </div>
           <button class="icon-btn" title="Menú" (click)="isCollapsed = !isCollapsed">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-              <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"/>
+              <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40ZM40,152H56a8,8,0,0,0,0-16H40V120H56a8,8,0,0,0,0-16H40V88H56a8,8,0,0,0,0-16H40V56H80V200H40Zm176,48H96V56H216V200Z"></path>
             </svg>
           </button>
         </div>
